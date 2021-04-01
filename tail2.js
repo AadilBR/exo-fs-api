e215603aa1454452dfe4bd18dc61e9ef9a6bca3a
AadilBR <aadil.iphone@gmail.com>
2021-04-01 20:57:28 +0200

    tail2.js still in progress

--- tail2.js
+++ tail2.js
@@ -1,4 +1,6 @@
 const fs = require('fs')
+
+let nbLines = 10
 let shift = 2
 
 //Gestion de l'erreur si pas le bon nombre d'arguments.
@@ -11,34 +13,38 @@ if (process.argv.length < 3 || process.argv.length > 5) {
 if (process.argv.length === 5) {
 
   if ((process.argv[2] === '-n')) {
-    if (!isNaN(process.argv[3])) {
+    if (isNaN(process.argv[3])) {
       console.log(process.argv[3])
-      console.log(`Error: if you choose an option you must write it this way [-n number]`)
+      console.log(`Error: if you choose an option you must write it this way [-n number] fileName`)
       process.exit(01)
     }
   }
 }
+
+nbLines = Number(process.argv[3])
+shift += 2
+
 //Gestion de l'erreur si AmaFille.txt n'existe pas.
-if (!fs.existsSync(process.argv[4])) {
-  console.log(4)
-  console.log(`Error: ${process.argv[2]} does not exist`)
+if (!fs.existsSync(process.argv[shift])) {
+  console.log(process.argv[shift])
+  console.log(`Error: ${process.argv[shift]} does not exist`)
   process.exit(1)
 }
 
 //Si il existe verifier que c'est un fichier et non un répertoire
-const stats = fs.statSync(process.argv[2])
+const stats = fs.statSync(process.argv[shift])
 if (!stats.isFile()) {
-  console.log(`Error: ${process.argv[2]} is not a file`)
+  console.log(`Error: ${process.argv[shift]} is not a file`)
   process.exit(1)
 }
 
 //Lire le fichier
-let txt = fs.readFileSync(process.argv[2], 'utf-8')
+let txt = fs.readFileSync(process.argv[shift], 'utf-8')
 //Faire le traitement attendu : split slice ...
 const tab = txt.split('\n')
 
 
 
 //Affiche les 10 dernières lignes du fichier file.txt.
-console.log(tab.slice(-10).join('\n'))
+console.log(tab.slice(nbLines).join('\n'))
 //console.log(tab.slice(tab.length - 10))
